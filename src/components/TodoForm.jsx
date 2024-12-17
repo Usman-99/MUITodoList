5a01e55547377cad41902fe19fa35742d16724ab
Usman-99 <usmanyousuf1137@gmail.com>
2024-12-17 11:53:41 +0500

    using mui error and helper text

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,78 +1,10 @@
-// Todo form without formik and yup
-// import { Box, Typography } from "@mui/material";
-// import Grid from "@mui/material/Grid2";
-// import TextInput from "../components/TextInput";
-// import CustomButton from "../components/CustomButton";
-// const TodoForm = ({ title, setTitle, description, setDescription, submitHandler, errors }) => {
-//   return (
-//     <Box
-//       component="form"
-//       onSubmit={submitHandler}
-//       sx={{ width: "100%", mx: "auto", mt: 4,pt:2,pb:2,bgcolor:"white",borderRadius: 1}}
-//     >
-//       <Grid container rowSpacing={2} columnSpacing={2} justifyContent="center" >
-
-//         <Grid size={{ xs: 12, sm: 6, md: 4}}>
-//           <TextInput
-//             Multiline={false}
-//             Label="Enter a Todo..."
-//             Value={title}
-//             Onchange={(e) => setTitle(e.target.value)}
-//             SX={{ bgcolor: "background.paper", borderRadius: 1}}
-//             Type="text"
-//             Name="title"
-//           />
-//           {errors.title && (
-//             <Typography color="error" sx={{ fontSize: "0.875rem" }}>
-//               {errors.title}
-//             </Typography>
-//           )}
-//         </Grid>
-
-//         {/* Description input */}
-//         <Grid size={{ xs: 12, sm: 6, md: 4 }}>
-//           <TextInput
-//             Multiline={false}
-//             Label="Description"
-//             Value={description}
-//             Onchange={(e) => setDescription(e.target.value)}
-//             SX={{ bgcolor: "background.paper", borderRadius: 1 }}
-//             Type="text"
-//             Name="description"
-//           />
-//           {errors.description && (
-//             <Typography color="error" sx={{ fontSize: "0.875rem" }}>
-//               {errors.description}
-//             </Typography>
-//           )}
-//         </Grid>
-
-//         {/* Add Todo Button */}
-//         <Grid display="flex" justifyContent="center" maxHeight="3.5rem">
-//           <CustomButton
-//             Size="small"
-//             Type="submit"
-//             Variant="contained"
-//             Color="secondary"
-//             SX={{ fontSize: "1rem", fontWeight: "bold" }}
-//             Text="Add Todo"
-//           />
-//         </Grid>
-//       </Grid>
-//     </Box>
-//   );
-// };
-
-// export default TodoForm;
-
-// todoform with formik and yup
 import { Box } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import TextInput from "../components/TextInput";
 import CustomButton from "../components/CustomButton";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import ErrorComponent  from "../utils/CommonFunc";
+// import ErrorComponent  from "../utils/CommonFunc";
 
 // Validation schema using Yup
 const validationSchema = Yup.object({
@@ -128,10 +60,12 @@ const TodoForm = ({ submitHandler }) => {
             Name="title"
             Onblur={formik.handleBlur}
             Color="secondary"
+            Error={formik.touched.title && Boolean(formik.errors.title)}
+            HelperText={formik.errors.title}
           />
-          {formik.touched.title && formik.errors.title && (
+          {/* {formik.touched.title && formik.errors.title && (
             <ErrorComponent Text={formik.errors.title} />
-          )}
+          )} */}
         </Grid>
 
         {/* Description input */}
@@ -146,10 +80,12 @@ const TodoForm = ({ submitHandler }) => {
             Name="description"
             Onblur={formik.handleBlur}
             Color="secondary"
+            Error={formik.touched.description && Boolean(formik.errors.description)}
+            HelperText={formik.errors.description}
           />
-          {formik.touched.description && formik.errors.description && (
+          {/* {formik.touched.description && formik.errors.description && (
             <ErrorComponent Text={formik.errors.description} />
-          )}
+          )} */}
         </Grid>
 
         {/* Add Todo Button */}
