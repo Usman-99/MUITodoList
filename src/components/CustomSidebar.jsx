dd8a92f0709aa79b5b48035b1092c6fc99501fc7
Usman-99 <usmanyousuf1137@gmail.com>
2024-12-17 14:47:52 +0500

    adding links in sidebar

--- src/components/CustomSidebar.jsx
+++ src/components/CustomSidebar.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+import { Link, useLocation } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import { pages } from '../constants/base'; // Assuming you have the same pages array for links
+
+const StyledLink = styled(Link)(() => ({
+  color: "white",
+  textDecoration: 'none',
+  display: 'block',
+  marginBottom: '10px',
+  '&:hover': {
+    textDecoration: 'underline',
+  },
+}));
 
 const CustomSidebar = () => {
+  const { pathname } = useLocation(); 
+
   return (
-    <Box sx={{ p: 3, bgcolor: 'secondary.light', borderRadius: 2 }}>
-      <Typography variant="h6" gutterBottom>
+    <Box sx={{ p: 3, bgcolor: 'info.light', borderRadius: 2 }}>
+      <Typography variant="h6" sx={{fontWeight:"bold"}} gutterBottom>
         Sidebar
       </Typography>
-      <Typography>
-        sidebar
-      </Typography>
+      <Box>
+        {pages.map((page, index) => (
+          <StyledLink 
+            to={page.path} 
+            key={index}
+            onClick={() => {
+              if (pathname === page.path) window.scrollTo(0, 0); 
+            }}
+          >
+            {page.name}
+          </StyledLink>
+        ))}
+      </Box>
     </Box>
   );
 };
