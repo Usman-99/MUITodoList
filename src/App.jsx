d8753dfca3a416812bb8a66b61ca63f62a29ebf4
Usman-99 <usmanyousuf1137@gmail.com>
2024-12-18 12:20:40 +0500

    rendering allroutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-import ResponsiveAppBar from "./layout/Header";
-import Todo from "./pages/Todo";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import { Provider } from "react-redux";
-import store from "./store/todostore";
-import "./App.css"
-import Footer from "./layout/Footer";
-import PersonData from "./pages/PersonData";
+import AllRoutes from "./routes/AllRoutes";
 const App = () => {
   return (
-    <Provider store={store}>
-    <Router>
-      <ResponsiveAppBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Todo" element={<Todo />} />
-        <Route path="/PersonDetails" element={<PersonData />} />
-      </Routes>
-      <Footer />
-    </Router>
-    </Provider>
+    <AllRoutes/>
   );
 };
 export default App;
