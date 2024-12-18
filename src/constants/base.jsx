c8363ddc1a41e493e7912f74b23b76c9d6e52010
Usman-99 <usmanyousuf1137@gmail.com>
2024-12-18 17:04:16 +0500

    updating

--- src/constants/base.jsx
+++ src/constants/base.jsx
@@ -1,1 +1,1 @@
-export const pages=[{name:"Home",path:"/"},{name:"Person Details",path:"/PersonDetails"},{name:"Add Todo",path:"/Todo"},{name:"Feedback",path:"/#FeedbackForm"}]
+export const pages=[{name:"Home",path:"/"},{name:"Feedback",path:"/#FeedbackForm"},{name:"Person Details",path:"/PersonDetails"},{name:"Add Todo",path:"/Todo"}]
