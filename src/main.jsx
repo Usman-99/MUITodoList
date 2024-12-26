5404322606707a0ab0a913b18efb9958671d317c
Usman-99 <usmanyousuf1137@gmail.com>
2024-12-26 10:09:34 +0500

    updating with redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./App.css";
 import App from "./App.jsx";
-import store from "./store/todostore.js";
+import { store, persistor } from "./store/todostore.js";
 import { Provider } from "react-redux";
-
+import { PersistGate } from "redux-persist/integration/react";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/store/todostore.js
+++ src/store/todostore.js
@@ -1,12 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
-import commonDataReducer from "./commonslice"
+import commonDataReducer from "./commonslice";
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+// Redux Persist Configuration
+const persistConfig = {
+  key: 'myapp', 
+  storage, 
+};
+
+// Persisted Reducer
+const persistedReducer = persistReducer(persistConfig, commonDataReducer);
+
+// Store with Persisted Reducer
 const store = configureStore({
   reducer: {
-    commonData:commonDataReducer
+    commonData: persistedReducer, 
   },
 });
 
-export default store;
+const persistor = persistStore(store);
 
-
+export { store, persistor };
