c575d7daeee36222c93a151d1ac30131928fea6b
Usman-99 <usmanyousuf1137@gmail.com>
2024-12-26 12:54:58 +0500

    writing encryption logic

--- src/store/todostore.js
+++ src/store/todostore.js
@@ -2,11 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import commonDataReducer from "./commonslice";
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
+import { encryptTransform } from 'redux-persist-transform-encrypt';
 // Redux Persist Configuration
+
 const persistConfig = {
   key: 'myapp', 
-  storage, 
+  storage, transforms: [
+    encryptTransform({
+      secretKey: import.meta.env.VITE_redux_secret_key,
+      // secretKey: process.env.REACT_APP_redux_secret_key,
+      onError: function (error) {
+        console.error(error)
+      },
+    }),
+  ],
 };
 
 // Persisted Reducer
